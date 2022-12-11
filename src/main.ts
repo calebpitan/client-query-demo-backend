87b4fd663e53826b06e687d6d0d335436c4f8b6f
Caleb Adepitan <calebpitan@gmail.com>
2022-12-11 12:49:32 +0100

    use app config rather than hardcoded ports

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
+import { Configuration } from './utils/config/config.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const appConfig = app
+    .get<ConfigService<Configuration>>(ConfigService)
+    .get<Configuration['app']>('app')!;
 
   const config = new DocumentBuilder()
     .setTitle('Client Query Demo Backend')
@@ -17,6 +22,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(appConfig.port);
 }
 bootstrap();
