91b89c2cecdf76f33b4c1480328d3fe221d22be1
Caleb Adepitan <calebpitan@gmail.com>
2022-12-14 07:08:03 +0100

    update: major scaffolding
    - load configurations
    - setup and connect to database
    - setup logger module
    - add a user module
    - apply middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { UserModule } from './user/user.module.js';
+import { configFactory, TypeOrmConfigService } from './utils/config/index.js';
+import { LoggerModule } from './utils/logger/index.js';
+import { HelmetMiddleware, MorganMiddleware } from './utils/middleware/index.js';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load: [configFactory], isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    LoggerModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MorganMiddleware, HelmetMiddleware).forRoutes('*');
+  }
+}
