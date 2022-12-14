4020b2583e3984d7a94dc953f6771eaf5cba6a7a
Caleb Adepitan <calebpitan@gmail.com>
2022-12-14 07:18:59 +0100

    update: improve and load necessary config

--- src/utils/config/config.ts
+++ src/utils/config/config.ts
@@ -5,7 +5,7 @@ export type Configuration = ReturnType<typeof configFactory>;
 const APP_NAME = 'client-query-demo-backend';
 const ALGO: jwt.Algorithm = 'RS256';
 
-const configFactory = () => ({
+export const configFactory = () => ({
   app: {
     name: APP_NAME,
     port: Number(process.env.PORT) || 5000,
@@ -18,9 +18,12 @@ const configFactory = () => ({
   gql: { playground: process.env.GRAPHQL_PLAYGROUND! === 'true' },
 
   database: {
-    uri: process.env.MONGO_CONNECT_STRING || `mongodb://localhost/${APP_NAME}`,
-    redisUri: process.env.REDIS_TLS_URL || process.env.REDIS_URL || 'redis://localhost:6379',
-    redisLabsUri: process.env.REDIS_LABS_URL,
+    mongo_uri: process.env.MONGO_CONNECT_STRING || `mongodb://localhost/${APP_NAME}`,
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '5432',
+    database_name: APP_NAME,
+    redis_uri: process.env.REDIS_TLS_URL || process.env.REDIS_URL || 'redis://localhost:6379',
+    redis_labs_uri: process.env.REDIS_LABS_URL,
   },
 
   auth: {
@@ -34,7 +37,9 @@ const configFactory = () => ({
       key: process.env.REDIS_LABS_PRIVATE_KEY,
       crt: process.env.REDIS_LABS_CERTICICATE,
     },
+    postgres: {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
   },
 });
-
-export default configFactory;
